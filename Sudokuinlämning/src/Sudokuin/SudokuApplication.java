64f28fecccef05536b132bb4ff45a4b092c81544
macbook <macbook@10.1.236.253>
2019-02-19 15:35:49 +0100

    javafx

--- Sudokuinlämning/src/Sudokuin/SudokuApplication.java
+++ Sudokuinlämning/src/Sudokuin/SudokuApplication.java
@@ -1,10 +1,19 @@
 package Sudokuin;
 
-public class SudokuApplication {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SudokuApplication extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
