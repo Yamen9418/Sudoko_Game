a8013ead59fcb16d1c136f4185fb818d31b97884
macbook <macbook@10.1.236.253>
2019-02-19 14:48:54 +0100

    Sudoku projektet

---
+++ Sudokuinlämning/src/Sudokuin/Sudoku.java
@@ -0,0 +1,5 @@
+package Sudokuin;
+
+public class Sudoku {
+
+}
---
+++ Sudokuinlämning/src/Sudokuin/SudokuApplication.java
@@ -0,0 +1,10 @@
+package Sudokuin;
+
+public class SudokuApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
